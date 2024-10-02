a3735e6223c9ebfbc92bc368ee7a1106e9a6e37f
suo3 <obiaproduction@gmail.com>
2024-10-02 10:56:18 -0400

    add files

---
+++ README.md
@@ -0,0 +1,12 @@
+_Implementing a Backend Using Express, Mongoose ODM and Jest_
+
+## Requirements
+
+Please install the following, if you do not already have them installed:
+
+- Node.js v20.10.0
+- Git v2.43.0
+- Visual Studio Code v1.84.2
+- Docker v24.0.6
+- Docker Desktop v4.25.2
+- MongoDB Shell v2.1.0
---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { postsRoutes } from './routes/posts.js'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+postsRoutes(app)
+
+postsRoutes(app)
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!')
+})
+
+export { app }
